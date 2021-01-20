5238ca2c66ce0f47a554872013d5d3f2f93e323c
felipe_rb <feliperb.stuff@gmail.com>
2021-01-20 03:27:57 -0600

    correccion leyenda para carga de archivo en la pantalla descifrar/verificar
    La leyenda para la carga de archivo se actualiza en función de la acción seleccionada. Si 'descifrar' está seleccionado se usa la leyenda "Cargar archivo cifrado.", en el caso contrario se usa la leyenda "Cargar archivo en claro."

--- User Interface V1/checkboxes_control.js
+++ User Interface V1/checkboxes_control.js
@@ -1,3 +1,6 @@
+var descifrar_check_state = 1;
+var verificar_check_state = 1;
+
 $(document).ready(function() { 
     $('input[type="checkbox"]').click(function() { 
 
@@ -9,7 +12,25 @@ $(document).ready(function() {
         }else{
             var inputValue = $(this).attr("value"); 
             $("." + inputValue).toggle(); 
+
+            if(inputValue=='VERIFICAR_CHECK'){
+                verificar_check_state=verificar_check_state*-1;
+                //console.log(verificar_check_state)
+            }
+
+            if(inputValue=='DESCIFRAR_CHECK'){
+                descifrar_check_state=descifrar_check_state*-1;
+                //console.log(descifrar_check_state)
+            }
+
+            if(descifrar_check_state==1){
+                /*Cargar archivo cifrado*/
+                $('#label_archivo strong').text('archivo cifrado');
+            }else{
+                /*Cargar archivo en claro*/
+                $('#label_archivo strong').text('archivo en claro');
+            }
+
         }
-        
     }); 
 }); 
