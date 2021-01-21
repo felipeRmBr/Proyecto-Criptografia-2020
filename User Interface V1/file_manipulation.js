1b1413b078406b08cf32b2d6525bca8a60c31d50
felipe_rb <feliperb.stuff@gmail.com>
2021-01-21 13:05:48 -0600

    lectura de archivos
    primeras pruebas para la lectura de archivos (WORKING)

--- User Interface V1/file_manipulation.js
+++ User Interface V1/file_manipulation.js
@@ -1,9 +1,28 @@
 
+var reader; //GLOBAL File Reader object for demo purpose only
+var plain_text;  // base64 formar
+var simmetric_key;
+var public_key;
+var decryptedText;
+var file_name;
+
 // feature detection for drag&drop upload
 var isAdvancedUpload = true
 
 // applying the effect for every form
 var forms = document.querySelectorAll( '.box' );
+
+// Check API SUPPORT
+function checkFileAPI() {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        reader = new FileReader();
+        console.log('reader created')
+        return true; 
+    } else {
+        alert('The File APIs are not fully supported by your browser. Fallback required.');
+        return false;
+    }
+}
 
 Array.prototype.forEach.call( forms, function( form )
 {
@@ -12,16 +31,82 @@ Array.prototype.forEach.call( forms, function( form )
         errorMsg     = form.querySelector( '.box__error span' ),
         restart      = form.querySelectorAll( '.box__restart' ),
         droppedFiles = false,
-        showFiles    = function( files )
+        file_class = form.getAttribute('file_class'),
+        reader = new FileReader(),
+        showFiles    = function(files)
         {
             /*Muestra el nombre del archivo cargado*/
             label.textContent = files.length > 1 ? ( input.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', files.length ) : files[ 0 ].name;
-        };
+        },
+
+        // Cargar archivo
+        readSelectedFile = function(fileSelection) 
+        {
+            console.log('runnig read_selected_file()')
+            var output = ""; //placeholder for text output
+            if(fileSelection.files && fileSelection.files[0]) {  
+
+                //reader = new FileReader();
+
+                reader.onload = function (e) {
+                    if(file_class=='plain_file'){
+                        plain_text = e.target.result;  //base64 formar
+                        //console.log(cadena_original)
+                        //output = e.target.result.split(',')[1];
+                        //output = cadena_original;
+                        displayContents('COMPLETE!!');
+
+                        console.log('Cadena original');
+                    }
+
+                    if(file_class=='simmetric_key'){
+                        simmetric_key = e.target.result;  //base64 formar
+                        //console.log(cadena_original)
+                        //output = e.target.result.split(',')[1];
+                        //output = cadena_original;
+                        displayContents(simmetric_key);
+
+                        console.log('Simmetric key');
+                    }
+
+                    if(file_class=='public_key'){
+                        public_key = e.target.result;  //base64 formar
+                        //console.log(cadena_original)
+                        //output = e.target.result.split(',')[1];
+                        //output = cadena_original;
+                        displayContents(public_key);
+
+                        console.log('Public Key');
+                    }
+                    //var element = document.getElementById('main'); 
+                    //element.innerHTML = cadena_original; //display output in DOM
+                };//end onload()
+
+                reader.onprogress = function(data) {
+                    if (data.lengthComputable) {                                            
+                        var progress = parseInt( ((data.loaded / data.total) * 100), 10 );
+                        console.log(progress);
+                    }
+                };
+
+                reader.readAsDataURL(fileSelection.files[0]);
+
+                file_name = fileSelection.files[0].name;
+                //file_extension = file_name.split('.')[1];
+            }
+            else { //this is where you could fallback to Java Applet, Flash or similar
+                console.log('No se leyo ningún archivo')
+                return false;
+
+            }       
+            return true;
+        };   
 
     //Muestra los archivos cargados
-    input.addEventListener( 'change', function( e )
+    input.addEventListener( 'change', function( element )
     {
-        showFiles( e.target.files );
+        readSelectedFile(element.target);
+        showFiles(element.target.files);
     });
 
     
@@ -63,4 +148,9 @@ Array.prototype.forEach.call( forms, function( form )
     });
 
 });
+
+function displayContents(txt) {
+    var element = document.getElementById('main'); 
+    element.innerHTML = txt; //display output in DOM
+}  
 
